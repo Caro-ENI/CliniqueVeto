bf659539dc37072472e019fa6d07a6aab9366da5
Caro-ENI <38671074+Caro-ENI@users.noreply.github.com>
2018-04-29 19:08:06 +0200

    Ajout d'une lib externe pour le JDatePicke Ajout du JDatePicker dans Prise de RDV Ajout des ComboBox des Heures/Minutes dans Prise de RDV Rangement de la Classe Prise de RDV

--- src/fr/eni/cach/clinique/ihm/priseRDV/PriseRDVPanel.java
+++ src/fr/eni/cach/clinique/ihm/priseRDV/PriseRDVPanel.java
@@ -7,29 +7,37 @@ import java.awt.FlowLayout;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
-import javax.swing.JFormattedTextField;
 import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
 import javax.swing.border.TitledBorder;
 
+import org.jdatepicker.JDatePicker;
+
 import fr.eni.cach.clinique.ihm.UtilsIHM;
 import fr.eni.cach.clinique.ihm.cliniqueVeto.CliniqueVetoFrame2;
-import fr.eni.cach.clinique.ihm.connexion.ConnexionFrame;
 import fr.eni.cach.clinique.ihm.ecranAnimaux.AnimauxPanel;
 import fr.eni.cach.clinique.ihm.ecranClients.AjoutClientPanel;
-import fr.eni.cach.clinique.ihm.ecranClients.GestClientPanel;
 
 public class PriseRDVPanel extends JPanel {
+	/*
+	 * ORGANISATION DE LA CLASSE : 
+	 * - déclaration des attributs 
+	 * - constructeur principal 
+	 * - création des panels 
+	 * - creation des boutons 
+	 * - méthodes annexes 
+	 * - getters
+	 */
+
+	// *********** ATTRIBUTS ***************
 
 	private static final long serialVersionUID = 7834116385228727053L;
 	
@@ -81,23 +89,26 @@ public class PriseRDVPanel extends JPanel {
 	private JLabel jlDate;
 	private JLabel jlHeure;
 	private JLabel jlH;
-	
-	private JFormattedTextField jtfDate;
-	
+		
 	private JComboBox<String> jcbVeto;
 	//TODO changer en Client et en Animal !! 
 	private JComboBox<String> jcbClient;
 	private JComboBox<String> jcbAnimal;
 	
+	private JComboBox<Integer> jcbHeures;
+	private JComboBox<Integer> jcbMinutes;
+	
+	private JPanel panelHeure;
+	
 	private UtilsIHM utilsIHM = UtilsIHM.getInstance();
-	
-	
 	
 	/**
 	 * modèle de JTable disposant les données dans la JTable
 	 */
 	private TableRDVModel modelTablRDV;
 	
+	
+	// *********** CONSTRUCTEUR ***************
 	
 	/**
 	 * Constructeur de la fenêtre Prise de RDV (3)
@@ -107,25 +118,34 @@ public class PriseRDVPanel extends JPanel {
 	 */
 	public PriseRDVPanel() {
 			
-		//création de la JTable & Boutons & JComboBox & JLabels & JtextField
+		// création de la JTable
+		this.createTableRDV();
+		
+		// création des Boutons 
 		this.createBttOK();
 		this.createBttSuppr();
-		this.createTableRDV();
 		this.createBttModif();
+		this.createBttAjoutClient();
+		this.createBttAjoutAniaml();
+		
+		// création des JComboBox 
 		this.createJcbVeto();
 		this.createJcbAnimal();
 		this.createJcbClient();
-		this.createBttAjoutClient();
-		this.createBttAjoutAniaml();
+		this.createJcbHeures();
+		this.createJcbMinutes();
+		
+		// création des JLabels 
 		this.createJlClient();
 		this.createJlAnimal();
 		this.createJlVeto();
 		this.createJlDate();
 		this.createJlHeure();
 		this.createJlH();
-		this.createJtfDate();
+
 		
 		//création des panels
+		this.createPanelHeure();
 		this.createPanelPour();
 		this.createPanelPar();
 		this.createPanelQuand();
@@ -146,8 +166,7 @@ public class PriseRDVPanel extends JPanel {
 	}
 
 	
-	// *********** CREATE **************
-	
+	// *********** CREATE PANELS **************
 	/**
 	 * Crée le panel d'informations du RDV à partir de 3 sous-panels
 	 * panelPour
@@ -189,10 +208,77 @@ public class PriseRDVPanel extends JPanel {
 	}
 	
 	/**
+	 * Crée le panel Pour : Renseigne les infos du client grâce à des JComboBox
+	 * nom du client
+	 * nom de son animal 
+	 * Si l'une des données n'est pas en BDD il est possible de la créer à la volée 
+	 * -> liens vers (4) et (6)
+	 */
+	private void createPanelPour() {
+		panelPour = new JPanel(new GridBagLayout());
+		
+		//met une Bordure au Panel et lui donne un titre
+		panelPour.setBorder(new TitledBorder("Pour : "));
+		
+		utilsIHM.addComponentTo(getJlClient(), panelPour, 0, 0, 1, 1, 0.2, false);
+		utilsIHM.addComponentTo(getJcbClient(), panelPour, 0, 1, 1, 1, 0.8, true);
+		utilsIHM.addComponentTo(getBttAjoutClient(), panelPour, 1, 1, 1, 1, 0.2, false);
+		
+		utilsIHM.addComponentTo(getJlAnimal(), panelPour, 0, 2, 1, 1, 0.2, false);
+		utilsIHM.addComponentTo(getJcbAnimal(), panelPour, 0, 3, 1, 1, 0.8, true);
+		utilsIHM.addComponentTo(getBttAjoutAnimal(), panelPour, 1, 3, 1, 1, 0.2, false);
+		
+	}
+	
+	/**
+	 * Crée le panel Par : renseigne sur le véto grâce à des JComboBox
+	 * par défaut donne le premier veto disponible (à la première date disponible)
+	 */
+	private void createPanelPar() {
+		panelPar = new JPanel(new GridBagLayout());
+		panelPar.setBorder(new TitledBorder("Par : "));
+		
+		utilsIHM.addComponentTo(getJlVeto(), panelPar, 0, 0, 1, 1, 0.2, true);
+		utilsIHM.addComponentTo(getJcbVeto(), panelPar, 0, 1, 1, 1, 0.8, true);
+	
+	}
+	
+	/**
+	 * Crée le panel Quand : renseigne la date du Rdv
+	 * Date rentrée dans un champ ou grâce à une JDatePicker
+	 * Heure avec 2 Jcombobox (heures et minutes) -> seuelement tous les 15min de dispo pour les mins
+	 * par défaut donne le premier veto disponible (à la première date/heure disponible)
+	 */
+	private void createPanelQuand() {
+		panelQuand = new JPanel(new GridBagLayout());
+		panelQuand.setBorder(new TitledBorder("Quand : "));
+		
+		utilsIHM.addComponentTo(getJlDate(), panelQuand, 0, 0, 1, 1, 0.2, true);
+		utilsIHM.addComponentTo(new JDatePicker(), panelQuand, 0, 1, 1, 1, 0.5, true);
+		
+		utilsIHM.addComponentTo(getJlHeure(), panelQuand, 0, 2, 1, 1, 0.2, true);
+		utilsIHM.addComponentTo(getPanelHeure(), panelQuand, 0, 3, 1, 1, 0.8, true);
+	}
+	
+	private void createPanelHeure() {
+		panelHeure = new JPanel();
+		BoxLayout layout = new BoxLayout(panelHeure, BoxLayout.X_AXIS);
+		panelHeure.setLayout(layout);
+		panelHeure.add(getJcbHeures());
+		panelHeure.add(getJlH());
+		panelHeure.add(getJcbMinutes());
+
+
+	}
+
+
+	// *********** CREATE JTABLE **************
+	
+	/**
 	 * Crée la Table des RDV à partir d'un modèle de données
 	 * doit être réactif à la date saisie dans le panelQuand
 	 */
-	private void createTableRDV(){
+ 	private void createTableRDV(){
 		modelTablRDV = new TableRDVModel();
 		
 		tableRDV = new JTable(modelTablRDV);
@@ -207,6 +293,9 @@ public class PriseRDVPanel extends JPanel {
 		 tableArticle.getSelectionModel().addListSelectionListener(new List...
 		 */
 	}
+
+	
+	// *********** CREATE BOUTONS **************
 	
 	/**
 	 * Crée le bouton valider 
@@ -233,84 +322,6 @@ public class PriseRDVPanel extends JPanel {
 		bttModif = new JButton("Modifier");
 	}
 
-	/**
-	 * Crée le panel Pour : Renseigne les infos du client grâce à des JComboBox
-	 * nom du client
-	 * nom de son animal 
-	 * Si l'une des données n'est pas en BDD il est possible de la créer à la volée 
-	 * -> liens vers (4) et (6)
-	 */
-	private void createPanelPour() {
-		panelPour = new JPanel(new GridBagLayout());
-		
-		//met une Bordure au Panel et lui donne un titre
-		panelPour.setBorder(new TitledBorder("Pour : "));
-		
-		utilsIHM.addComponentTo(jlClient, panelPour, 0, 0, 1, 1, 0.2, false);
-		utilsIHM.addComponentTo(jcbClient, panelPour, 0, 1, 1, 1, 0.8, true);
-		utilsIHM.addComponentTo(bttAjoutClient, panelPour, 1, 1, 1, 1, 0.2, false);
-		
-		utilsIHM.addComponentTo(jlAnimal, panelPour, 0, 2, 1, 1, 0.2, false);
-		utilsIHM.addComponentTo(jcbAnimal, panelPour, 0, 3, 1, 1, 0.8, true);
-		utilsIHM.addComponentTo(bttAjoutAnimal, panelPour, 1, 3, 1, 1, 0.2, false);
-		
-	}
-	
-	/**
-	 * Crée le panel Par : renseigne sur le véto grâce à des JComboBox
-	 * par défaut donne le premier veto disponible (à la première date disponible)
-	 */
-	private void createPanelPar() {
-		panelPar = new JPanel(new GridBagLayout());
-		panelPar.setBorder(new TitledBorder("Par : "));
-		
-		utilsIHM.addComponentTo(jlVeto, panelPar, 0, 0, 1, 1, 0.2, true);
-		utilsIHM.addComponentTo(getJcbVeto(), panelPar, 0, 1, 1, 1, 0.8, true);
-	
-	}
-	
-	/**
-	 * Crée le panel Quand : renseigne la date du Rdv
-	 * Date rentrée dans un champ ou grâce à une JDatePicker
-	 * Heure avec 2 Jcombobox (heures et minutes) -> seuelement tous les 15min de dispo pour les mins
-	 * par défaut donne le premier veto disponible (à la première date/heure disponible)
-	 */
-	private void createPanelQuand() {
-		panelQuand = new JPanel(new GridBagLayout());
-		panelQuand.setBorder(new TitledBorder("Quand : "));
-		
-		utilsIHM.addComponentTo(jlDate, panelQuand, 0, 0, 1, 1, 0.2, false);
-		utilsIHM.addComponentTo(jtfDate, panelQuand, 0, 1, 1, 1, 0.8, true);
-		utilsIHM.addComponentTo(new JLabel("datePicker"), panelQuand, 1, 1, 1, 1, 0.2, false);
-		
-		utilsIHM.addComponentTo(jlHeure, panelQuand, 0, 2, 1, 1, 0.2, false);
-		utilsIHM.addComponentTo(new JLabel("toto"), panelQuand, 0, 3, 1, 1, 0.8, true);
-	}
-	
-	private void createJcbVeto(){
-		//TODO remplir avec la liste des veto courante
-		String[] veto = {"Veto1", "Veto2", "Veto3", "Veto4", "Veto5"};
-		jcbVeto = new JComboBox<>(veto);
-		//permet de ne pas sélectionner par défaut le nom d'un véto
-		jcbVeto.setSelectedItem(null);
-	}
-	
-	private void createJcbClient(){
-		//TODO remplir avec la liste des veto courante
-		String[] veto = {"Client1", "Client2", "Client3", "Client4", "Client5"};
-		jcbClient = new JComboBox<>(veto);
-		//permet de ne pas sélectionner par défaut le nom d'un véto
-		jcbClient.setSelectedItem(null);
-	}
-	
-	private void createJcbAnimal(){
-		//TODO remplir avec la liste des veto courante
-		String[] veto = {"Animal1", "Animal2", "Animal3", "Animal4", "Animal5"};
-		jcbAnimal = new JComboBox<>(veto);
-		//permet de ne pas sélectionner par défaut le nom d'un véto
-		jcbAnimal.setSelectedItem(null);
-	}
-	
 	private void createBttAjoutClient(){
 		bttAjoutClient = new JButton("Ajout");
 		bttAjoutClient.addActionListener(new ActionListener() {
@@ -348,10 +359,10 @@ public class PriseRDVPanel extends JPanel {
 		
 	}
 	
-	private void createJlClient(){
-		jlClient = new JLabel("Client");
-	}
+
+
 	
+	// *********** CREATE JLABELS **************
 	private void createJlAnimal(){
 		jlAnimal = new JLabel("Animal");
 	}
@@ -372,10 +383,48 @@ public class PriseRDVPanel extends JPanel {
 		jlH = new JLabel("h"); 
 	}
 	
-	private void createJtfDate() {
-	//	DateFormat monFormat = DateFormat.SHORT;
-		jtfDate = new JFormattedTextField("DD/MM/YYYY");
+	private void createJlClient(){
+		jlClient = new JLabel("Client");
 	}
+
+	// *********** CREATE JCOMBOBOX **************	
+		
+	private void createJcbVeto(){
+		//TODO remplir avec la liste des veto courante
+		String[] veto = {"Veto1", "Veto2", "Veto3", "Veto4", "Veto5"};
+		jcbVeto = new JComboBox<>(veto);
+		//permet de ne pas sélectionner par défaut le nom d'un véto
+		jcbVeto.setSelectedItem(null);
+	}
+	
+	private void createJcbClient(){
+		//TODO remplir avec la liste des veto courante
+		String[] veto = {"Client1", "Client2", "Client3", "Client4", "Client5"};
+		jcbClient = new JComboBox<>(veto);
+		//permet de ne pas sélectionner par défaut le nom d'un véto
+		jcbClient.setSelectedItem(null);
+	}
+	
+	private void createJcbAnimal(){
+		//TODO remplir avec la liste des veto courante
+		String[] veto = {"Animal1", "Animal2", "Animal3", "Animal4", "Animal5"};
+		jcbAnimal = new JComboBox<>(veto);
+		//permet de ne pas sélectionner par défaut le nom d'un véto
+		jcbAnimal.setSelectedItem(null);
+	}
+
+	
+	private void createJcbHeures() {
+		Integer[] heures = {8,9,10,11,12,13,14,15,16,17,18,19};
+		jcbHeures = new JComboBox<Integer>(heures);
+	}
+
+	private void createJcbMinutes() {
+		Integer[] minutes = {00,15,30,45};
+		jcbMinutes = new JComboBox<Integer>(minutes);
+	}
+	
+
 	
 	// *********** GETTERS **************
 	
@@ -464,5 +513,57 @@ public class PriseRDVPanel extends JPanel {
 
 	public JComboBox<String> getJcbVeto(){
 		return jcbVeto;
+	}
+
+	public JLabel getJlClient() {
+		return jlClient;
+	}
+	
+	public JComboBox<String> getJcbClient() {
+		return jcbClient;
+	}
+	
+	public JButton getBttAjoutClient() {
+		return bttAjoutClient;
+	}
+	
+	public JLabel getJlAnimal() {
+		return jlAnimal;
+	}
+	
+	public JComboBox<String> getJcbAnimal() {
+		return jcbAnimal;
+	}
+
+	public JButton getBttAjoutAnimal() {
+		return bttAjoutAnimal;
+	}
+
+	public JLabel getJlVeto() {
+		return jlVeto;
+	}
+
+	public JLabel getJlDate() {
+		return jlDate;
+	}
+	
+	public JLabel getJlHeure() {
+		return jlHeure;
+	}
+
+	public JLabel getJlH() {
+		return jlH;
+	}
+	
+	public JComboBox<Integer> getJcbHeures(){
+		return jcbHeures;
+	}
+	
+	public JComboBox<Integer> getJcbMinutes(){
+		return jcbMinutes;
+	}
+	
+	public JPanel getPanelHeure() {
+		return panelHeure;
 	}
 }
